c45a99cec367702d40cb56b9e769e97ec95c4924
Brian Pipa <brianpipa@gmail.com>
2023-01-08 17:57:32 -0500

    initial commit

---
+++ src/test/java/com/pipasoft/overreader/OverreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipasoft.overreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
